3dc8f40d09548c3093a85538fdb2e8b88ebf671d
Ivan <91627086+kami-ivan@users.noreply.github.com>
2025-09-12 21:07:45 +0300

    Второе домашнее задание

--- hw02/src/main/java/ru/otus/hw/Application.java
+++ hw02/src/main/java/ru/otus/hw/Application.java
@@ -1,14 +1,20 @@
 package ru.otus.hw;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import ru.otus.hw.service.TestRunnerService;
+import ru.otus.hw.service.TestRunnerServiceImpl;
 
+@PropertySource("classpath:application.properties")
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
 
         //Создать контекст на основе Annotation/Java конфигурирования
-        ApplicationContext context = null;
-        var testRunnerService = context.getBean(TestRunnerService.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+        var testRunnerService = context.getBean(TestRunnerServiceImpl.class);
         testRunnerService.run();
 
     }
--- hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,10 +1,16 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
 
+import java.util.List;
+
+@Service
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
@@ -18,10 +24,33 @@ public class TestServiceImpl implements TestService {
         ioService.printFormattedLine("Please answer the questions below%n");
         var questions = questionDao.findAll();
         var testResult = new TestResult(student);
+        int questionNumber = 1;
 
-        for (var question: questions) {
+        ioService.printLine("13 questions with 4 answer options, but only one is correct. " +
+                "After each question, enter the number of the correct answer.\n");
+
+        for (var question : questions) {
             var isAnswerValid = false; // Задать вопрос, получить ответ
+            ioService.printFormattedLine("Question %d: %s", questionNumber, question.text());
+
+            List<Answer> answers = question.answers();
+            if (!CollectionUtils.isEmpty(answers)) {
+                for (int answerNumber = 0; answerNumber < answers.size(); answerNumber++) {
+                    Answer answer = answers.get(answerNumber);
+                    ioService.printFormattedLine("  %d. %s", answerNumber + 1, answer.text());
+                }
+            } else {
+                ioService.printLine("  No answers available");
+            }
+
+            int selectedAnswer = ioService.readIntForRangeWithPrompt(1, 4,
+                    "Enter the correct answer number",
+                    "There is no such answer, so enter it again.");
+
+            isAnswerValid = answers.get(selectedAnswer - 1).isCorrect();
+
             testResult.applyAnswer(question, isAnswerValid);
+            questionNumber++;
         }
         return testResult;
     }
