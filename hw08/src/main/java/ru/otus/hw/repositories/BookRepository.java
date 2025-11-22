ac710ede05dfb2deef20d6e3eedc3d98fb37067c
Ivan <ivan.pavl0v@yandex.ru>
2025-11-22 12:51:29 +0300

    Восьмое дз. Клонирование седьмого.

---
+++ hw08/src/main/java/ru/otus/hw/repositories/BookRepository.java
@@ -0,0 +1,18 @@
+package ru.otus.hw.repositories;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    @EntityGraph(value = "book-author-genre-graph")
+    Optional<Book> findById(long id);
+
+    @NonNull
+    @EntityGraph(value = "book-author-genre-graph")
+    List<Book> findAll();
+}
