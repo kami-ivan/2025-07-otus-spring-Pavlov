cff33ebfbd8b884a46655b857c86671b08b6a4a7
Ivan <ivan.pavl0v@yandex.ru>
2025-12-05 17:10:27 +0300

    Тринадцатое дз. Сдача на проверку 2.

--- hw13/src/main/java/ru/otus/hw/security/SecurityConfiguration.java
+++ hw13/src/main/java/ru/otus/hw/security/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,8 +22,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authz -> authz
+        return httpSecurity.authorizeHttpRequests(authz -> authz
                         .requestMatchers("/login", "/css/**", "/js/**").permitAll()
                         .requestMatchers("/add/book", "/edit/book").hasRole("ADMIN")
 
